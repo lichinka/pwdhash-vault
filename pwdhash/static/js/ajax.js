d8f32c7dd00a422cd06e2c6bdc47540f9ed0f72a
Lucas Benedicic <lucas.benedicic@gmail.com>
2015-01-09 11:35:31 +0100

    Model overlay working ok.-

--- pwdhash/static/js/ajax.js
+++ pwdhash/static/js/ajax.js
@@ -1,4 +1,22 @@
 /**
+ * Deletes the selected entry from the vault.-
+ */
+function deleteKey ( )
+{
+    var delete_field = document.getElementById("delete");
+    //
+    // we use the delete field to know which entry should be deleted
+    //
+    delete_field.value = document.getElementById("name").value;
+    //
+    // send the form
+    //
+    document.getElementById("hash_form").submit ( );
+    return false;
+}
+
+
+/**
  * Forwards the selected entry to the corresponding form target.-
  */
 function sendEntry (element)
@@ -14,3 +32,23 @@ function sendEntry (element)
     document.getElementById("entries_form").submit ( );
     return false;
 }
+
+
+/**
+ * Displays a modal dialog with a semi-transparent overlay.-
+ */
+function displayDialog ( )
+{
+    var modal= document.getElementById('modal');
+    var shade= document.getElementById('shade');
+    if (modal != null && shade != null)
+    {
+        modal.style.display = 'block';
+        shade.style.display = 'block';
+        document.getElementById('close_dialog').onclick= function() {
+            modal.style.display=shade.style.display= 'none';
+        };
+    }
+}
+ 
+window.onload = displayDialog;
