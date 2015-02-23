4eef9b77977a7ca8f01f43493508faa76c3e5879
Lucas Benedicic <lucas.benedicic@gmail.com>
2015-02-23 11:21:03 +0100

    Setup script updated.-

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup, find_packages
 
 
 
-version = '0.2.0'
+version = '0.0.1'
 
 
 def read (fname):
     """
-    Utility function to read the README file; used for the long_description.-
+    Returns the content of 'fname' as a string.-
     """
     return open (os.path.join (os.path.dirname (__file__), fname)).read ( )
 
@@ -42,10 +42,18 @@ def check_for_sqlite ( ):
     return ret_value
 
 
+def get_requirements (fname):
+    """
+    Gets a list of requirements from the content of 'fname'.-
+    """
+    req = read ('requirements.txt') % check_for_sqlite ( )
+    return req.split ('\n')
 
-setup (name='pwdhash',
+
+
+setup (name='pwdhash-vault',
        version=version,
-       description="An implementation of Stanford's PwdHash",
+       description="A local password vault using Stanford's PwdHash",
        long_description=read ('README.md'),
        classifiers=['Development Status :: 5 - Production/Stable',
                     'Environment :: Console',
@@ -62,18 +70,14 @@ setup (name='pwdhash',
                     'Programming Language :: Python :: 2.6',
                     'Programming Language :: Python :: 2.7',],
        keywords='local pwdhash vault',
-       author='Lev Shamardin, Lucas Benedicic',
-       author_email='shamardin@gmail.com, lucas.benedicic@gmail.com',
-       url='https://github.com/abbot/pwdhash',
+       author='Lucas Benedicic',
+       author_email='lucas.benedicic@gmail.com',
+       url='https://github.com/lichinka/pwdhash-vault',
        license='BSD',
        py_modules=['main'],
        packages=['pwdhash'],
        include_package_data=True,
-       install_requires=['CherryPy', 
-                         'Jinja2', 
-                         'SQLObject', 
-                         'apsw<=%s' % check_for_sqlite ( ), 
-                         'nose'],
+       install_requires=get_requirements ('requirements.txt'),
        entry_points={ 'console_scripts': [ 'pwdhash = main:main' ] },
        zip_safe=False,
       )
